7253fbfc30e2cbd905e56ed540534e05a76f10b0
elisha-16 <ghoshelisha19@gmail.com>
2021-11-18 21:27:00 +0530

    added carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import {Carousel} from 'react-bootstrap'
+import img4 from '../../images/img4.jpg'
+import img1 from '../../images/headphone.jpg'
 
 function Home() {
     return (
         <div>
-            <h1>Home component</h1>
+          <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="First slide"
+      height={400}
+    />
+    
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="Second slide"
+      height={400}
+    />
+    
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="Third slide"
+      height={400}
+    />
+    
+  </Carousel.Item>
+</Carousel>
         </div>
     )
 }
